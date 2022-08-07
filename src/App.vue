7f637dd3b3441e147d20673b871609a6f23529b2
jai bajamundi <jaibajamundi1@gmail.com>
2022-08-07 16:46:18 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,173 @@
+<template>
+<div class="head">
+ <p class="text">Hello Developer!</p>
+ <p class="smalltext">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+</div> 
+  <main id="app">
+    <section class="products"></section>
+    <ProductCard
+      v-for="product in products"
+      :product="product"
+    />
+
+  </main>
+</template>
+
+<script>
+import ProductCard from './components/Product';
+export default {
+  name: 'App',
+  components: {
+    ProductCard
+  },
+  data() {
+    return{
+     products:[
+       {
+       title: "card1",
+       src: require('./assets/smallcards.png')
+       },
+       {
+       title: "card2",
+       src: require('./assets/smallcards.png')
+       },
+       {
+       title: "card3",
+       src: require('./assets/smallcards.png')
+       },
+     ] 
+    }
+  }
+}
+</script>
+
+<style>
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  
+}
+
+
+
+ 
+
+@media screen and (min-width: 600px) {
+
+  body{
+  background-image: url("./assets/header1.png");
+  font-family: 'monseratt', sans-serif;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  font-size:1vw;
+  
+
+  }
+  
+  main {
+  width: 100vw;
+  min-height: 1vh;
+  overflow:hidden;
+
+
+  display:flex;
+  justify-content: center;
+  align-items:center; 
+  }
+
+  .text{
+  color:#fff;
+  font-size:5vw;
+  font-family: arial;
+  text-align: center;
+  padding-top: 1.5em;
+  
+  }
+
+  .text:hover{
+    color:black;
+    cursor: pointer;
+  }
+
+  .smalltext{
+  color:#fff;
+  font-size:2vw;
+  font-family: arial;
+  text-align: center;
+  padding-top: 1.5em;
+
+  }
+
+  .head {
+  background: rgba(0,0,0,0.4);
+  height: auto;
+  padding-top: 6%;
+  padding-bottom:9%;
+  }
+  
+  
+}
+
+
+@media screen and (max-width:600px) {
+  body{
+  
+  background-image: url("./assets/header2.png");
+  font-family: 'monseratt', sans-serif;
+  background-repeat: no-repeat;
+  background-size: 100% auto;
+  font-size:1em;
+
+
+  }
+
+  main {
+
+  min-height: 100vh;
+  overflow:hidden;
+  width: 100%;
+  display: block;
+  margin-bottom: 20px;
+
+  }
+
+   .text{
+  color:#fff;
+  font-size:15vw;
+  font-family: arial;
+  text-align: center;
+  padding-top: 1.5em;
+  font-weight: 550;
+  
+ .text:hover{
+    color:black;
+    cursor: pointer;
+  }
+
+  }
+
+  .smalltext{
+  color:#fff;
+  font-size:8vw;
+  font-family: arial;
+  text-align: center;
+  padding-top: 1.5em;
+  padding-right:5px;
+   padding-left: 20px;
+
+
+  }
+
+  .head {
+  background: rgba(0,0,0,0.4);
+  height: auto;
+  padding-bottom:6%;
+  }
+  
+  
+}
+
+
+
+</style>
---
+++ src/components/Product.vue
@@ -0,0 +1,102 @@
+<template>
+    
+    <div class="product">
+        <div class="product-inner">
+            <div class="product-image-wrap">
+                <img :src="product.src" class="image" />
+            </div> 
+            <div class="product-detail">
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae efficitur enim. Integer at sem at elit pulvinar placerat. Proin fermentum eget dolor in efficitur. 
+                 Etiam turpis nulla, pharetra a sodales auctor, interdum eu orci.</p>
+                <button class="button">READ MORE</button>
+            </div>
+        </div> 
+        
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'product',
+    props: ['product']
+}
+</script>
+
+
+<style>
+.product {
+    flex: 1 1 33.333%;
+    width: 100%;
+    padding: 25px;
+    margin-bottom:5%;
+    
+}
+
+.product:hover{
+    box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.50);
+}
+
+.products {
+  display:flex;
+  max-width:1288px;
+  padding:25px;
+  
+}
+
+.product-inner{
+  position: relative;
+  padding: 25px;
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  border-radius: 8px;
+}
+
+.product-image-wrap .image{
+  width:90%;
+  display: block;  
+  margin-left: auto;  
+  margin-right: auto; 
+}
+
+.product-image-wrap .image:hover{
+ width:100%;
+ display: block;  
+ margin-left: auto;  
+ margin-right: auto; 
+}
+
+.product-detail {
+    text-align:center;
+}
+
+#p1,#p2{
+   height:25px;
+   width:25px;
+   border-radius:50% 0 0 50%;
+   background-color:blue;  
+}
+
+#p2{
+   border-radius:0 50% 50% 0;
+}
+   
+.button {
+  background-color: white;
+  border-style: solid;
+  color: black;
+  padding: 10px 20px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  margin:6px 2px;
+  cursor: pointer;
+  border-radius: 16px;
+  font-weight: bold;
+}
+
+.button:hover {
+  background-color: #f1f1f1;
+}
+
+
+</style>
